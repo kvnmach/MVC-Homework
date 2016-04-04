55f36e00c1f2d4526bf2ce49d685ba9fa2ce1715
kvnmach <kvnmach@gmail.com>
2016-04-04 15:26:01 -0500

    added properties to classmate class

--- MVC Homework/Models/ClassMate.cs
+++ MVC Homework/Models/ClassMate.cs
@@ -7,5 +7,19 @@ namespace MVC_Homework.Models
 {
     public class ClassMate
     {
+        public class ClassMate
+        {
+            public ClassMate(int id, string name, string hair, double tall)
+            {
+                Id = id;
+                Name = name;
+                HairColor = hair;
+                Height = tall;
+            }
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string HairColor { get; set; }
+            public double Height { get; set; }
+        }
     }
 }
