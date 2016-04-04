c9c082c7ed35a6d380a2274b53c0a2b2b36c84cd
kvnmach <kvnmach@gmail.com>
2016-04-04 15:59:31 -0500

    added list on index

--- MVC Homework/Controllers/HomeController.cs
+++ MVC Homework/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MVC_Homework.Models;
 
 namespace MVC_Homework.Controllers
 {
@@ -10,7 +11,18 @@ namespace MVC_Homework.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            List<ClassMate> ClassMates = new List<ClassMate>()
+            {
+                new ClassMate(1, "Seth", "Black", 5.0),
+                new ClassMate(2, "Brain", "Black", 5.0),
+                new ClassMate(2, "Zach", "Brown", 5.0),
+                new ClassMate(2, "Trey", "Stoner", 5.0),
+                new ClassMate(2, "Kate", "Brown", 5.0),
+                new ClassMate(2, "Tee", "Brown", 5.0),
+                new ClassMate(2, "Kevin", "Brown", 5.0),
+            };
+            return View(ClassMates);
+            
         }
 
         public ActionResult About()
