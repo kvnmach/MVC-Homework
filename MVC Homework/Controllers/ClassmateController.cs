321a7c527f8cb08cb8de953691868f5f27d0ac21
kvnmach <kvnmach@gmail.com>
2016-04-25 22:08:26 -0500

    transferred list of classmates to classmate controller

--- MVC Homework/Controllers/ClassmateController.cs
+++ MVC Homework/Controllers/ClassmateController.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Collections.Generic;
 using System.Web.Mvc;
+using MVC_Homework.Models;
 
 namespace MVC_Homework.Controllers
 {
@@ -11,7 +9,17 @@ namespace MVC_Homework.Controllers
         // GET: Classmate
         public ActionResult Index()
         {
-            return View();
+            var classmates = new List<ClassMate>
+            {
+                new ClassMate(1, "Seth", "Black", 5.0),
+                new ClassMate(2, "Brain", "Black", 5.0),
+                new ClassMate(2, "Zach", "Brown", 5.0),
+                new ClassMate(2, "Trey", "Stoner", 5.0),
+                new ClassMate(2, "Kate", "Brown", 5.0),
+                new ClassMate(2, "Tee", "Brown", 5.0),
+                new ClassMate(2, "Kevin", "Brown", 5.0)
+            };
+            return View(classmates);
         }
     }
 }
