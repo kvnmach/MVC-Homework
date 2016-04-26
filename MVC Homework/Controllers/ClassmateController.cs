8c123bf65bca7b54bcf01b32ec87a1fb446550f0
kvnmach <kvnmach@gmail.com>
2016-04-25 22:05:35 -0500

    added classmate controller

---
+++ MVC Homework/Controllers/ClassmateController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Homework.Controllers
+{
+    public class ClassmateController : Controller
+    {
+        // GET: Classmate
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
